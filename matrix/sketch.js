c06fb3ccb138e6c28b43e10d793a5a4e8e0c0972
Gaurav Singh <gauravsingh@Gauravs-MacBook-Air.local>
2017-10-22 23:23:58 +0530

    english matrix

---
+++ matrix/sketch.js
@@ -0,0 +1,98 @@
+
+
+
+var symbolSize=26;
+var streams=[];
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(0);
+  var x=0;
+  for(var i=0;i<=width/symbolSize;i++){
+    stream=new Stream();
+
+    stream.generateSymbols(x,random(-1000,0));
+    streams.push(stream);
+    x+=symbolSize+20;
+
+  }
+
+
+  textSize(symbolSize);
+}
+
+function draw() {
+  background(0,150);
+  streams.forEach(function(stream){
+      stream.rander();
+  })
+
+
+}
+
+function Symbol(x,y,speed,first){
+this.x=x;
+this.y=y;
+this.speed=speed;
+this.value;
+this.first=first;
+this.switchInterval=round(random(2,20));
+this.setToRandomSymbol=function(){
+  if(frameCount % this.switchInterval==0){
+    this.value=String.fromCharCode(
+      27 + round(random(0,100))
+    );
+  }
+
+
+}
+this.rander=function(){
+  fill(0,255,70);
+  text(this.value,this.x,this.y);
+  this.rain();
+  this.setToRandomSymbol();
+}
+
+this.rain=function(){
+if(this.y>=height){
+  this.y=0;
+}else{
+  this.y+=this.speed;
+}
+
+
+
+}
+
+
+}
+
+
+function Stream (){
+  this.symbols=[];
+  this.totalSymbols=round(random(5,50));
+  this.speed=random(1,10);
+  this.generateSymbols=function(x,y){
+    var first=round(random(0,4))==1;
+    for(var i=0;i<=this.totalSymbols;i++){
+      var symbol=new Symbol(x,y,this.speed,first);
+      symbol.setToRandomSymbol();
+      this.symbols.push(symbol);
+      y-=symbolSize;
+      first=false;
+    }
+  }
+  this.rander=function(){
+
+    this.symbols.forEach(function(symbol){
+      if(symbol.first){
+        fill(180,255,180);
+      }else{
+          fill(0,255,70);
+      }
+
+      text(symbol.value,symbol.x,symbol.y);
+      symbol.rain();
+      symbol.setToRandomSymbol();
+    });
+  }
+}
